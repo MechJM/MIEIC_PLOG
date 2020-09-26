0b2ff3082f82856cc0fb6e38c8fe988b4363768d
MechJM <matosjoaops@protonmail.com>
2020-09-26 23:43:22 +0100

    Started ex2

---
+++ Aula 1/ex2.pl
@@ -0,0 +1,15 @@
+pilot(Lamb).
+pilot(Besenyei).
+pilot(Chambliss).
+pilot(MacLean).
+pilot(Mangold).
+pilot(Jones).
+pilot(Bonhomme).
+
+team(Lamb,Breitling).
+team(Besenyei,"Red Bull").
+team(Chambliss,"Red Bull").
+team(MacLean,"Mediterranean Racing Team").
+team(Mangold,Cobra).
+team(Jones,Matador).
+team(Bonhomme,Matador).
