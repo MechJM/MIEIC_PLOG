526dd8392ede5dbd8e8043339babe46409597bb7
MechJM <matosjoaops@protonmail.com>
2020-09-27 13:21:28 +0100

    Did ex3

---
+++ Aula 1/ex3.pl
@@ -0,0 +1,12 @@
+livro(os_maias).
+escreveu(eca,os_maias).
+nacionalidade(eca,portugues).
+autor(os_maias,eca).
+tipo(os_maias,romance).
+tipo(os_maias,ficcao).
+
+/* Queries
+a) autor(os_maias,A).
+b) nacionalidade(A,portugues),escreveu(A,B),tipo(B,romance).
+c) autor(A,B),livro(A),tipo(A,ficcao),autor(C,B),livro(C),C\=A,tipo(C,D),D\=ficcao.
+*/
