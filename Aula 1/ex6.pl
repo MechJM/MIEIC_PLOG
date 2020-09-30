5481a9d57c299865c88dbfef18810c678039dbc4
MechJM <matosjoaops@protonmail.com>
2020-09-30 10:27:32 +0100

    Started solving ex6

---
+++ Aula 1/ex6.pl
@@ -0,0 +1,22 @@
+passaro(tweety).
+
+peixe(goldie).
+
+minhoca(molie).
+
+gato(silvester).
+
+gosta_de(Elem1,Elem2) :-
+    passaro(Elem1),
+    minhoca(Elem2).
+
+gosta_de(Elem1,Elem2) :-
+    gato(Elem1),
+    peixe(Elem2).
+
+gosta_de(Elem1,Elem2) :-
+    gato(Elem1),
+    passaro(Elem2).
+
+gosta_de(Amigo1,Amigo2) :-
+    amigo(Amigo1,Amigo2)
