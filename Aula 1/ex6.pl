574fce465c2a84f6fdea7844eb32a87d25f27681
MechJM <matosjoaops@protonmail.com>
2020-10-04 12:54:21 +0100

    Did ex6

--- Aula 1/ex6.pl
+++ Aula 1/ex6.pl
@@ -6,6 +6,10 @@ minhoca(molie).
 
 gato(silvester).
 
+gosta_de(Amigo1,Amigo2) :-
+    amigo(Amigo1,Amigo2);
+    amigo(Amigo2,Amigo1).
+
 gosta_de(Elem1,Elem2) :-
     passaro(Elem1),
     minhoca(Elem2).
@@ -18,6 +22,11 @@ gosta_de(Elem1,Elem2) :-
     gato(Elem1),
     passaro(Elem2).
 
-gosta_de(Amigo1,Amigo2) :-
-    amigo(Amigo1,Amigo2);
-    amigo(Amigo2,Amigo1).
+amigo(eu,silvester).
+
+come(silvester,Alimento) :-
+    gosta_de(silvester,Alimento).
+
+/* Queries
+come(silvester,X).
+*/
