32cab5f46596ac55e864d104a0318d6d9626527e
MechJM <matosjoaops@protonmail.com>
2020-10-04 15:29:35 +0100

    Did ex9

--- Aula 1/ex9.pl
+++ Aula 1/ex9.pl
@@ -12,8 +12,41 @@ funcionario(pedro, ist).
 funcionario(ana_paula, feup).  
 funcionario(carlos, feup).
 
+aluno_do_professor(Aluno,Professor) :-
+    professor(Professor,Disciplina),
+    aluno(Aluno,Disciplina).
+
+pessoa_da_universidade(Pessoa,Universidade) :-
+    frequenta(Pessoa,Universidade);
+    funcionario(Pessoa,Universidade).
+
+colega(Pessoa1,Pessoa2) :-
+(
+    (
+        professor(Pessoa1,_),
+        professor(Pessoa2,_),
+        funcionario(Pessoa1,Faculdade),
+        funcionario(Pessoa2,Faculdade)
+    );
+    (
+        (
+            frequenta(Pessoa1,Faculdade),
+            frequenta(Pessoa2,Faculdade)
+        );
+        (
+            aluno(Pessoa1,Disciplina),
+            aluno(Pessoa2,Disciplina)
+        )
+        /* Como ver se são do mesmo curso?
+        (
+
+        )*/
+    )
+),
+    Pessoa1 \== Pessoa2.
+
 /* Queries
-a) professor(X,Disciplina),aluno(Aluno,Disciplina).
-b) 
-c) 
+a) aluno_do_professor(Aluno,X).
+b) pessoa_da_universidade(Pessoa,X).
+c) colega(Pessoa1,Pessoa2).
 */
