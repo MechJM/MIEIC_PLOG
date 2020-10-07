0935fece99665d83e18e8c1f032a448a74e03212
MechJM <matosjoaops@protonmail.com>
2020-10-07 09:43:25 +0100

    Did ex2

--- Aula 2/ex1.pl
+++ Aula 2/ex1.pl
@@ -7,3 +7,17 @@ s(b,c).
 s(b,d).
 s(c,c).
 s(d,e).
+
+/* Respostas 
+1)
+X=a
+Y=d
+Z=e
+
+2)
+a,b 2
+a,c 1
+b,a 0
+a,d 0
+Total = 3
+*/
--- Aula 2/ex2.pl
+++ Aula 2/ex2.pl
@@ -7,3 +7,11 @@ b(N,b3).
 c(X,Y) :- a(X,N), b(N,Y).
 d(X,Y) :- a(X,N), b(Y,N).
 d(X,Y) :- a(N,X), b(N,Y).
+
+/* Respostas
+a) yes
+b) X = 2
+c) X = a1
+d) X = a1, Y = b1; X = a1, Y = b3
+e) X = a1, Y = 2
+*/
