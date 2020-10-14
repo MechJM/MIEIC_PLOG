a23670f38964cea64b744239acc36045e190fe81
MechJM <matosjoaops@protonmail.com>
2020-10-14 09:16:49 +0100

    Added class 3 directory and did ex1

---
+++ Aula 3/ex1.pl
@@ -0,0 +1,14 @@
+/* 
+a) Sim
+b) Não
+c) H = apple, T = [broccoli, refrigerator]
+d) H = a, T = [b,c,d,e]
+e) H = apples, T = [bananas]
+f) H = a, T = [[b,c,d]]
+g) H = apples, T = []
+h) Não
+i) One = apple, Two = sprouts,T = [fridge,milk]
+j) X = a,Z = [Y|T]
+k) H = apple, T = [Z]
+l) Sim
+*/
